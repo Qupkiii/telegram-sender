9e8d471a2f9cf2d76c90fa8b448c389240d172cc
Qupkiii <na8991@gmail.com>
2022-04-27 20:28:24 +0200

    first functions

---
+++ routes/telegram.js
@@ -0,0 +1,24 @@
+require('dotenv').config()
+
+const express = require('express')
+const TelegramBot = require('node-telegram-bot-api');
+const router = express.Router()
+const bot = require('../telegramBot')
+
+
+//initialize telegram bot
+
+
+//Sending message
+router.post('/',  (req, res) => {
+    bot.sendMessage(req.body.userID, req.body.msg)
+        .then((response) => {
+            res.status(200).json({'response': 'successfully sent message', 'msg': req.body.msg})
+        })
+        .catch(err => {
+            res.status(500).json({'response': 'ERROR', 'msg': err.toString()})
+        })
+    //res.send(req.body.userID.toString() + "; " + req.body.msg.toString())
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+mongoose.connect(process.env.DATABESE_URL_MONGO)
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((error) => console.log(error.toString()))
+const db = mongoose.connection
+db.on('error', (error) => console.error(error.toString()))
+db.once('open', () => console.log('Connected to DB'))
+
+app.use(express.json())
+
+const telegramRouter = require('./routes/telegram')
+const {response} = require("express");
+app.use('/telegram', telegramRouter)
+
+const bot = require('./telegramBot')
+const fetch = require("node-fetch");
+
+bot.onText(/(Show\s)?Kitty*/, (msg) => {
+    const chatId = msg.chat.id;
+
+    let url = 'https://api.thecatapi.com/v1/images/search'
+    fetch(url)
+        .then(response => response.json())
+        .then((data) => {
+            bot.sendMessage(chatId, data[0].url)
+                //.then(response => console.log(response))
+                //.catch(err => console.error(err))
+            console.log(data[0].url)
+        })
+        .catch((err) => {
+            bot.sendMessage(chatId, err.toString())
+            //console.log(err.toString())
+        })
+});
+
+app.listen(process.env.PORT, () => console.log('server started'))
